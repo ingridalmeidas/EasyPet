f7b838262b8d12754d70aad5981344d4b3cd4356
Valfredo <valfredojr3@gmail.com>
2018-06-15 12:13:48 -0300

    Aplicando o padrão facade para simplificar a classe de interface.

---
+++ src/view/Facade.java
@@ -0,0 +1,136 @@
+import business.control.*;
+import java.util.Scanner;
+
+public class Facade {
+  private GerenciaUsuario gerenteUsuario;
+  private GerenciaAnimal gerenteAnimal;
+  private GerencialAdocao gerenteAdocao;
+   
+  public void inicializarSistema() {
+    gerenteUsuario = GerenciaUsuario.getInstancia();
+    try {
+  	  gerenteUsuario.leLista();
+  	}catch(ArquivoNaoEncontrado e) {
+  		System.out.println("\n" + e.getMessage());
+  	}
+        
+    gerenteAnimal = GerenciaAnimal.getInstancia();
+    try {
+  	  gerenteAnimal.leLista();
+   	}catch(ArquivoNaoEncontrado e) {
+   		System.out.println("\n" + e.getMessage());
+   	}
+      
+    gerenteAdocao = GerenciaAdocao.getInstancia();
+    try {
+  		gerenteAdocao.leLista();
+  	}catch(ArquivoNaoEncontrado e) {
+   		System.out.println("\n" + e.getMessage());
+   	}
+  }
+    
+  public void finalizarSistema() {
+    System.out.println("\nAté mais!");
+    try {
+      gerenteUsuario.salvaListaEmArquivo();
+    }catch(ArquivoNaoEncontrado e) {
+      System.out.println("\n" + e.getMessage());
+    }
+    
+    try {
+		  gerenteAnimal.salvaListaEmArquivo();
+		}catch(ArquivoNaoEncontrado e) {
+		 System.out.println("\n" + e.getMessage());
+		}
+        
+    try {
+		  gerenteAdocao.salvaListaEmArquivo();
+		}catch(ArquivoNaoEncontrado e) {
+		 System.out.println("\n" + e.getMessage());
+		}
+  }
+    
+  public void listaDeUsuarios() {
+    System.out.println("\nLista de usuários em arquivo: ");
+    gerenteUsuario.listaDeUsuarios();
+  }
+  
+  public void listaDeAnimais() {
+    System.out.println("\nLista de animais em arquivo: ");
+    gerenteAnimal.listaDeAnimais();
+  }
+  
+  public void listaDeAdocoes() {
+    System.out.println("\nLista de animais em arquivo: ");
+    gerenteAdocao.listaDeAdocoes();
+  }
+  
+  public void cadastrarUsuario() {
+    Scanner s = new Scanner(System.in);
+    Usuario usuario = new Usuario();
+    
+    boolean loginValido = false;
+	  boolean senhaValida = false; 
+      
+    while(!loginValido) {
+        
+      System.out.println("\nDigite seu Login: ");
+       
+      usuario.setLogin(s.nextLine());
+        
+      try {
+        loginValido = gerenteUsuario.validaLogin(usuario);
+      }catch(LoginInvalido e) {
+        System.out.println("\nLogin invalido! " + e.getMessage() + " Digite novamente!");
+      }
+    }
+      
+    while(!senhaValida) {
+      System.out.println("\nDigite sua senha: ");
+      
+      usuario.setSenha(s.nextLine());
+      
+      try {
+        senhaValida = gerenteUsuario.validaSenha(usuario);
+      }catch(SenhaInvalida e) {
+        System.out.println("\nSenha invalida! " + e.getMessage() + " Digite novamente!");
+      }
+    }
+        
+    System.out.println("\nDigite seu nome:");
+    usuario.setNome(s.nextLine());
+        
+    System.out.println("\nDigite sua idade:");
+    usuario.setIdade(s.nextInt());
+    s.nextLine();
+        
+    System.out.println("\nDigite seu sexo: F - Feminino, M - Masculino");
+    usuario.setSexo(s.nextLine().charAt(0));
+       
+    System.out.println("\nDigite sua cidade:");
+    usuario.setCidade(s.nextLine());
+        
+    System.out.println("\nDigite seu Estado:");
+    usuario.setEstado(s.nextLine());
+      
+    System.out.println("\nDigite palavras chaves separadas por espaço com características de um animal que você deseja adotar:");
+    usuario.setCaracteristicasAnimais(s.nextLine());
+      
+    gerenteUsuario.adicionaUsuario(usuario);
+  }
+  
+  public void removeUsuario(){
+    String loginRemovido = "";
+    
+    System.out.println("\nDigite o login do usuario que voce deseja excluir: ");
+    loginRemovido = s.nextLine();
+    
+    if (gerenteUsuario.removeUsuario(loginRemovido)) {
+      System.out.println("\nUsuario removido: " + loginRemovido);
+    }
+    else{
+      System.out.println("\nUsuario nao foi removido");
+    }
+  }
+  
+}
--- src/view/Interface.java
+++ src/view/Interface.java
@@ -2,7 +2,7 @@ package view;
 
 import java.util.Scanner;
 
-import business.control.*;
+import Facade;
 
 import business.model.Usuario;
 import util.LoginInvalido;
@@ -22,107 +22,29 @@ public class Interface {
 		
 		Scanner s = new Scanner(System.in);
 		Usuario usuario = new Usuario();
-		GerenciaUsuario gerencia = new GerenciaUsuario();
+		Facade fachada = new Facade();
 		
-		String loginRemovido = "";
+		fachada.inicializarSistema();
 		
-		try {
-			gerencia.leLista();
-		}catch(ArquivoNaoEncontrado e) {
-			System.out.println("\n" + e.getMessage());
-		}
-		
-		
-		System.out.println("\nLista de usuários em arquivo: ");
-		gerencia.listaDeUsuarios();
+		fachada.listaDeUsuarios();
 		
 		while(continua) {
 			
-			boolean loginValido = false;
-			boolean senhaValida = false;
-			
 			System.out.println("\n\nO que voce deseja fazer? 1-Adicionar usuario 2-Excluir usuario 3-Fechar o programa");
-			
 			decisao = s.nextInt();
 			s.nextLine();
 			
 			if (decisao == 1) {
-				
-				while(!loginValido) {
-					
-					System.out.println("\nDigite seu Login: ");
-					
-					usuario.setLogin(s.nextLine());
-					
-					try {
-						loginValido = gerencia.validaLogin(usuario);
-					}catch(LoginInvalido e) {
-						System.out.println("\nLogin invalido! " + e.getMessage() + " Digite novamente!");
-					}
-				}
-				
-				while(!senhaValida) {
-					System.out.println("\nDigite sua senha: ");
-					
-					usuario.setSenha(s.nextLine());
-					
-					try {
-						senhaValida = gerencia.validaSenha(usuario);
-					}catch(SenhaInvalida e) {
-						System.out.println("\nSenha invalida! " + e.getMessage() + " Digite novamente!");
-					}
-				}
-					
-				System.out.println("\nDigite seu nome:");
-				usuario.setNome(s.nextLine());
-					
-				System.out.println("\nDigite sua idade:");
-				usuario.setIdade(s.nextInt());
-				s.nextLine();
-					
-				System.out.println("\nDigite seu sexo: F - Feminino, M - Masculino");
-				usuario.setSexo(s.nextLine().charAt(0));
-					
-				System.out.println("\nDigite sua cidade:");
-				usuario.setCidade(s.nextLine());
-					
-				System.out.println("\nDigite seu Estado:");
-				usuario.setEstado(s.nextLine());
-				
-				System.out.println("\nDigite palavras chaves separadas por espaço com características de um animal que você deseja adotar:");
-				usuario.setCaracteristicasAnimais(s.nextLine());
-				
-				gerencia.adicionaUsuario(usuario);
-				
-				
-				
+			  fachada.cadastrarUsuario();
 			}
 			
 			else if (decisao == 2) {
-				System.out.println("\nDigite o login do usuario que voce deseja excluir: ");
-				loginRemovido = s.nextLine();
-				
-				if (gerencia.removeUsuario(loginRemovido)) {
-					System.out.println("\nUsuario removido: " + loginRemovido);
-				}
-				
-				else{
-					System.out.println("\nUsuario nao foi removido");
-				}
+				fachada.removeUsuario();
 			}
 			
 			else if (decisao == 3){
-				
-				System.out.println("\nLista de usuários em arquivo: ");
-				gerencia.listaDeUsuarios();
-				
-				System.out.println("\nAté mais!");
-				try {
-					gerencia.salvaListaEmArquivo();
-				}catch(ArquivoNaoEncontrado e) {
-					System.out.println("\n" + e.getMessage());
-				}
-				
+				fachada.listaDeUsuarios();
+				fachada.finalizarSistema();
 				continua = false;
 			}
 			
